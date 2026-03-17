90c5c9d93ee07b50297a7b1a91cf50b32a884274
Your Name <you@example.com>
2026-03-17 10:39:26 +0100

    wq

---
+++ tests/rooms.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+def first_room(driver):
+     username_input = driver.find_element(By.XPATH, '//*[@id="root"]/main/div[1]/input[1]')
+     username_input.send_keys("admin")
+
+     password_input = driver.find_element(By.XPATH, '//*[@id="root"]/main/div[1]/input[2]')
+     password_p = driver.find_element(By.CSS_SELECTOR, '#root > main > div.flip-card > div > div.flip-card-back > p:nth-child(3)')
+     pwd = password_p.get_attribute("textContent")
+     password_input.send_keys(pwd)
+     
+     login_btn = driver.find_element(By.TAG_NAME, "button")
+     login_btn.click()
+     
+     print("Logged in successfully!")
